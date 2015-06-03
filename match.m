775691ca314e08638995b95fcf035f7d5eeed132
Adar Hefer <adarhefer@localhost.localdomain>
2015-06-03 22:56:57 +0300

    match works! :D

--- match.m
+++ match.m
@@ -6,25 +6,34 @@ function [num_matches,matches,dist_vals] = match(image1,image2, distRatio)
 [im1, des1, loc1] = sift(image1);
 [im2, des2, loc2] = sift(image2);
 
-matches_ret = 1:1;
-dist_vals_ret = 1:1;
+matches_ret = [];
+dist_vals_ret = [];
+pointsInIm1 = [;];
+pointsInIm2 = [;];
 
 for i = 1 : size(des1, 1)
-    cur = des1(i);
-    distances = zeros(size(des2, 1), 2);
+    cur = des1(i, :);
+    distances = zeros(size(des2, 1), 1);
+    min = flintmax;
+    minIndex = 1;
     for j = 1 : size(des2, 1)
-        distances(j) = [norm( cur - des2(j) ), j];
+        distances(j) = norm( cur - des2(j, :) );
+        if (distances(j) < min)
+            minIndex = j;
+            min = distances(j);
+        end
     end
     
     distances = sort(distances, 1);
     
     if distances(1, 1) / distances(2, 1) < distRatio
-        dist_vals_ret(end + 1) = distances(1);
-        matches_ret(end + 1) = [cur des2(distances(1), 2)];
+        dist_vals_ret = [dist_vals_ret; distances(1, 1) / distances(2, 1)];
+        pointsInIm1 = [pointsInIm1; loc1(i, 1:2)];
+        pointsInIm2 = [pointsInIm2; loc2(j, 1:2)];
     end
 end
 dist_vals = dist_vals_ret;
-matches = matches_ret;
+matches = horzcat(pointsInIm1, pointsInIm2, dist_vals_ret);
 num_matches = size(matches, 1);
 end
 
